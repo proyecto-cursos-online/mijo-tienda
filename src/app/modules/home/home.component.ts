9e955c5803c3264793f6e033df99890fa8a262ce
luisvilca10 <martinvilca@upeu.edu.pe>
2024-05-22 00:03:33 -0500

    Tienda category course descount

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,16 +3,23 @@ import { HomeService } from './services/home.service';
 
 declare var $: any;
 declare function HOMEINIT([]): any;
+declare function banner_home(): any;
+declare function countdownT(): any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent implements OnInit{
-  CATEGORIES:any = [];
-  COURSES_HOME:any = [];
-  group_courses_categories:any = [];
+export class HomeComponent implements OnInit {
+  CATEGORIES: any = [];
+  COURSES_HOME: any = [];
+  group_courses_categories: any = [];
+  desconut_baner: any = null;
+  discount_banner_course: any = [];
+  desconut_flash: any = null;
+  desconut_flash_course: any = [];
   constructor(
     public homerservice: HomeService
   ) {
@@ -21,11 +28,28 @@ export class HomeComponent implements OnInit{
     }, 50)
   }
   ngOnInit(): void {
-    this.homerservice.home().subscribe((resp:any)=>{
+    this.homerservice.home().subscribe((resp: any) => {
       console.log(resp)
       this.CATEGORIES = resp.categories;
       this.COURSES_HOME = resp.courses_home.data;
-      this.group_courses_categories = resp.group_courses_categories
+      this.group_courses_categories = resp.group_courses_categories;
+      this.desconut_baner = resp.desconut_baner;
+      this.discount_banner_course = resp.discount_banner_course;
+      this.desconut_flash = resp.desconut_flash;
+      this.desconut_flash_course=resp.desconut_flash_course
+      setTimeout(() => {
+        banner_home();
+        countdownT();
+      }, 50)
     })
+  }
+
+  getNewTotal(course: any, desconut_baner: any) {
+    if (desconut_baner.type_discount == 1) {
+      return course.precio_usd - course.precio_usd * (desconut_baner.discount * 0.01);
+    } else {
+      return course.precio_usd - desconut_baner.discount;
+    }
+
   }
 }
