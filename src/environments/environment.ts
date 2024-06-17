7f27a925bd5d3303838602092c5876e8c2701daf
luisvilca10 <martinvilca@upeu.edu.pe>
2024-06-17 01:24:35 -0500

    Mejoras Descuentos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  URL_BACKEND: 'http://127.0.0.1:8000/',
-  URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-  URL_FROTEND: 'http://localhost:5000',
+  URL_BACKEND: 'http://mijo-api.incadevs.com/',
+  URL_SERVICIOS: 'http://mijo-api.incadevs.com/api',
+  URL_FROTEND: 'http://mijo-tienda.incadevs.com',
   /*   URL_BACKEND: 'http://127.0.0.1:8000/',
     URL_SERVICIOS: 'http://127.0.0.1:8000/api',
     URL_FROTEND: 'http://localhost:5000', */
