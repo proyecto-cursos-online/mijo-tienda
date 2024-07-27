6fe43622881a9c15d86009c36de61087f64fd078
luisvilca10 <martinvilca@upeu.edu.pe>
2024-07-26 21:56:05 -0500

    Deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  URL_BACKEND: 'http://mijo-api.incadevs.com/',
-  URL_SERVICIOS: 'http://mijo-api.incadevs.com/api',
-  URL_FROTEND: 'http://mijo-tienda.incadevs.com',
-  /*   URL_BACKEND: 'http://127.0.0.1:8000/',
-    URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-    URL_FROTEND: 'http://localhost:5000', */
+  URL_BACKEND: 'https://mijo-api.incadevs.com/',
+  URL_SERVICIOS: 'https://mijo-api.incadevs.com/api',
+  URL_FROTEND: 'https://mijoschool.incadevs.com/',
+  // URL_BACKEND: 'http://127.0.0.1:8000/',
+  // URL_SERVICIOS: 'http://127.0.0.1:8000/api',
+  // URL_FROTEND: 'http://localhost:5000',
 };
